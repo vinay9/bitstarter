3655fc5e6eb364a41a7c1976272651160922e322
Vinay <vinayb211@gmail.com>
2013-07-11 05:23:24 +0000

    Read and print content of index.html

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('/index.html', function (err, data) {
+  if (err) throw err;
+var buffer = new Buffer(100);
+response.send(buffer.toString('utf8',0,data);  
+console.log(data);
+});
+
 });
 
 var port = process.env.PORT || 5000;
