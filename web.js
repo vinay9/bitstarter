f2d0938679bb699fd791b573a11ef798df80517c
Vinay <vinayb211@gmail.com>
2013-07-11 05:45:38 +0000

    Read and print content of index.html

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
 var buffer = new Buffer(data,'utf-8');
-response.send(buffer.toString('utf8'));  
+response.send(buffer.toString('utf-8'));  
 console.log(data);
 });
 
